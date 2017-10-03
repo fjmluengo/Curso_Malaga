526a24d49c4b4f4fa3d35196391e7d6bc0647288
fjmluengo <fjmluengo@gmail.com>
2017-10-03 13:52:48 +0200

    Modulos y componentes

---
+++ 03_modulos/componentes/src/app/app.module.ts
@@ -0,0 +1,23 @@
+// Módulos de Angular
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+// Módulos de la aplicación
+import { SharedModule } from './shared/shared.module';
+
+// Componentes del módulo
+import { AppComponent } from './app.component';
+import { LadoComponent} from './lado/lado.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LadoComponent
+  ],
+  imports: [
+    BrowserModule,
+    SharedModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ 03_modulos/componentes/src/app/lado/lado.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-lado',
+    template: `<div>lado</div>`
+})
+
+export class LadoComponent implements OnInit {
+    constructor() { }
+    ngOnInit() {
+    }
+
+}
